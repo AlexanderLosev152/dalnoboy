93846209fa7b323f3b91a6023850ff076b3da3f0
AlexanderLosev152 <losev.pavlovo@mail.ru>
2022-12-13 00:07:41 +0300

    textarea

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@
         mousewhell: true,
         keyboard: true,
       });
+
+      // // // кнопка прикрепить тз
+      // // window.onload = () => {
+
+      // //   const uploadFile = document.getElementById("upload__file");
+      // //   const uploadBtn = document.getElementById("upload__btn");
+      // //   const uploadText = document.getElementById("upload__text");
+
+      // //   // имитация клика
+      // //   uploadBtn.addEventListener('click', function () {
+      // //     uploadFile.click();
+      // //   })
+      // // }
+
+
+
+      // // кнопка Добавьте фото товара
+      // window.onload = () => {
+
+      //   const uploadFotoFile = document.getElementById("upload__foto-file");
+      //   const uploadFotoBtn = document.getElementById("upload__foto-btn");
+      //   const uploadFotoText = document.getElementById("upload__foto-text");
+
+      //   // имитация клика
+      //   uploadFotoBtn.addEventListener('click', function () {
+      //     uploadFotoFile.click();
+      //   })
+      // }      
